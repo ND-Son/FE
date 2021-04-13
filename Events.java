fd58353f2eae29e8a76d360355ff318aa16f71ea
Son Pham <sonerichpham@gmail.com>
2021-04-13 21:29:37 +0200

    units can attack each other and die (only the first attack unit though)

--- Events.java
+++ Events.java
@@ -4,13 +4,26 @@ public class Events {
 		return str;
 	}
 
+	public static byte counterAttack(byte str){
+		return str;
+	}
+
 	public static void fight(Unit blue, Unit red){
-		int hp = red.hp() - attack(blue.str());
-		if (hp <= 0){
-		red.setHP((byte)hp);
+		System.out.println(blue.name() + " is attacking " + red.name());
+		int hp_red = red.hp() - attack(blue.str());
+		int hp_blue = blue.hp();
+		if (hp_red <= 0){
+		red.setHP((byte)hp_red);
 		System.out.println(red.name()+" has been defeated");
 		}
-		else
-		System.out.println("red hp = " + red.hp());
+		else{
+		red.setHP((byte)hp_red);
+		System.out.println(red.name() + " hp = " + red.hp());
+
+		System.out.println(red.name() + " is counter attacking " + blue.name());
+		hp_blue -= attack(red.str());
+		blue.setHP((byte)hp_blue);
+		System.out.println(blue.name() + " hp = " + blue.hp());
+		}
 	}
 }
